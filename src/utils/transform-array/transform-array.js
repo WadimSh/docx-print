5ef2aefdb7284dcdd30314846aacaafc4b2e6b1e
WadimSh <wadim11@yandex.ru>
2024-06-19 17:35:36 +0300

    fix

--- src/utils/transform-array/transform-array.js
+++ src/utils/transform-array/transform-array.js
@@ -12,24 +12,42 @@ const transformArray = (data, profit, company, round) => {
     return Math.floor(number / factors[factors.length - 1]) * factors[factors.length - 1];
   };
 
-  const price1 = (value1, value2) => (value1 * (1 + value2 / 100)).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
-  const price2 = (value1, value2, value3) => (((value1 * (1 + value2 / 100)).toFixed(2)) * value3).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  const formatPrice = (price, profit, multiplicity, round) => {
+    let result = (price * (1 + profit / 100)).toFixed(2);
+    if (round) {
+      return result = (roundNumber(result * multiplicity) / multiplicity).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+    }
+    return result.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  };
 
-  const price1round = (value1, value2, value3) => (roundNumber((((value1 * (1 + value2 / 100)).toFixed(2)) * value3)) / value3).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
-  const price2round = (value1, value2, value3) => roundNumber((((value1 * (1 + value2 / 100)).toFixed(2)) * value3)).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
-  
+  const formatСost = (price, profit, multiplicity, round) => {
+    let result = (((price * (1 + profit / 100)).toFixed(2)) * multiplicity).toFixed(2);
+    if (round) {
+      return result = roundNumber(result).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+    }
+    return result.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  };
+
+  const getValue = (properties, ...names) => {
+    for (const name of names) {
+      const property = properties.find(prop => prop.name === name);
+      if (property?.value) return property.value;
+    }
+    return '';
+  };
+
   data.forEach(item => {
     const newObj = {
       image: item.images[0].image,
       company: company,
       code: item.code,
       name: item.name.replace(/\s+/g, ' '),
-      value: item.origin_properties.find(prop => prop.name === "Торговая марка")?.value || item.origin_properties.find(prop => prop.name === "Страна")?.value || '',
+      value: getValue(item.origin_properties, "Торговая марка", "Страна"),
       multiplicity: item.multiplicity,
       units1: item.units_counts[0] ? item.units_counts[0][0] : '',
       units2: item.units_counts[0] ? item.units_counts[0][1] : '',
-      price1: round ? price1round(item.measure_prices[0].price.currency_price, profit, item.multiplicity) : price1(item.measure_prices[0].price.currency_price, profit),
-      price2: round ? price2round(item.measure_prices[0].price.currency_price, profit, item.multiplicity) : price2(item.measure_prices[0].price.currency_price, profit, item.multiplicity),
+      price1: formatPrice(item.measure_prices[0].price.currency_price, profit, item.multiplicity, round),
+      price2: formatСost(item.measure_prices[0].price.currency_price, profit, item.multiplicity, round),
     };
     newArray.push(newObj);
   });
