410eb3df778e54c2bb1b0f37c609934a73d743e1
WadimSh <wadim11@yandex.ru>
2024-08-27 12:25:00 +0300

    fix

--- src/ui/img-preview/img-preview.jsx
+++ src/ui/img-preview/img-preview.jsx
@@ -6,8 +6,8 @@ const ImgPreview = ({ optionsArray, value }) => {
     <>
       {optionsArray.map((item) => (
         value === item.value && <>
-        <p style={{fontFamily: 'var(--font-family)', fontWeight: '400', fontSize: '14px', margin: '0  0 20px 0'}}>{item.description}</p>
-        <img src={item.imege_preview} alt={item.value} width='45%' /></>        
+        <p style={{fontFamily: 'var(--font-family)', fontWeight: '400', fontSize: '14px', margin: '0 30px 20px 0'}}>{item.description}</p>
+        <img src={item.imege_preview} alt={item.value} width='300px' /></>        
       ))}
     </>
   );
