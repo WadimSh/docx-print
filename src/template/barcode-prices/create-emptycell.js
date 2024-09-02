f9ff7230662a6db88c9b64669e68929a4fd8165f
WadimSh <wadim11@yandex.ru>
2024-09-02 15:14:07 +0300

    fix

--- src/template/barcode-prices/create-emptycell.js
+++ src/template/barcode-prices/create-emptycell.js
@@ -24,7 +24,7 @@ const createEmptyCell = () => {
     children: [
       new Table({
         width: {
-          size: 3750,
+          size: 3500,
           type: WidthType.DXA,
         },
         rows: [
