e177771e2b9bd77100323446f07e76efe995a573
WadimSh <wadim11@yandex.ru>
2024-09-25 13:45:14 +0300

    fix: оптимизирован компонент sidebar

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from "react";
+import { useState, useContext, useEffect, useMemo } from "react";
 import { StateContext, ConfigContext, MainContext } from "../../contexts/contexts";
 
 import SelectOptions from "../../ui/select-options/select-options";
@@ -29,86 +29,130 @@ const SideBar = () => {
   const config = useContext(ConfigContext);
   const data = useContext(MainContext);
   const { sharedValue, setSharedValue } = useContext(StateContext);
-  
-  const [logic, setLogic] = useState(false);
-  const [select, setSelect] = useState({});
-  const [check, setCheck] = useState({});
 
-  const [companyName, setCompanyName] = useState(sharedValue.companyName || defaultValues.companyName);
-  const [profit, setProfit] = useState(sharedValue.profit || defaultValues.profit);
-  const [round, setRound] = useState(sharedValue.round || defaultValues.round);
-  const [titleLabel, setTitleLabel] = useState(sharedValue.title || defaultValues.title);
-  const [companyLabel, setCompanyLabel] = useState(sharedValue.company || defaultValues.company);
-  const [requisitesLabel, setRequisitesLabel] = useState(sharedValue.requisites || defaultValues.requisites);
-  
+  const [state, setState] = useState({
+    logic: false,
+    select: {},
+    check: {},
+    companyName: sharedValue.companyName || defaultValues.companyName,
+    profit: sharedValue.profit || defaultValues.profit,
+    round: sharedValue.round || defaultValues.round,
+    titleLabel: sharedValue.title || defaultValues.title,
+    companyLabel: sharedValue.company || defaultValues.company,
+    requisitesLabel: sharedValue.requisites || defaultValues.requisites,
+  });
+
   const [newData, setNewData] = useState([]);
   const [metaData, setMetaData] = useState({});
 
+  const transformedData = useMemo(() => transformArray(data, state.profit, state.companyName, state.round), [data, state.profit, state.companyName, state.round]);
+
   useEffect(() => {
-    const transformedData = transformArray(data, profit, companyName, round);
     setNewData(transformedData);
-  }, [data, profit, companyName, round]);
+  }, [transformedData]);
 
   useEffect(() => {
     setMetaData({
-      title: titleLabel,
-      company: companyLabel,
-      requisites: requisitesLabel,
-    })
-  }, [titleLabel, companyLabel, requisitesLabel])
-    
+      title: state.titleLabel,
+      company: state.companyLabel,
+      requisites: state.requisitesLabel,
+    });
+  }, [state.titleLabel, state.companyLabel, state.requisitesLabel]);
+
   useEffect(() => {
-    setSharedValue(prevState => ({
+    setSharedValue((prevState) => ({
       ...prevState,
-      check: check.value,
-      companyName: companyName,
-      profit: profit,
-      round: round,
-      logic: logic,
-      title: titleLabel,
-      company: companyLabel,
-      requisites: requisitesLabel,
+      check: state.check.value,
+      companyName: state.companyName,
+      profit: state.profit,
+      round: state.round,
+      logic: state.logic,
+      title: state.titleLabel,
+      company: state.companyLabel,
+      requisites: state.requisitesLabel,
     }));
-  }, [logic, check, companyName, profit, round, select, titleLabel, companyLabel, requisitesLabel, setSharedValue]);
- 
+  }, [state]);
+
+  const handleSelect = (select) => {
+    setState((prevState) => ({ ...prevState, select }));
+  };
+
+  const handleCheck = (check) => {
+    setState((prevState) => ({ ...prevState, check }));
+  };
+
+  const handleLogic = (logic) => {
+    setState((prevState) => ({ ...prevState, logic }));
+  };
+
+  const handleCompanyName = (companyName) => {
+    setState((prevState) => ({ ...prevState, companyName }));
+  };
+
+  const handleProfit = (profit) => {
+    setState((prevState) => ({ ...prevState, profit }));
+  };
+
+  const handleRound = (round) => {
+    setState((prevState) => ({ ...prevState, round }));
+  };
+
+  const handleTitleLabel = (titleLabel) => {
+    setState((prevState) => ({ ...prevState, titleLabel }));
+  };
+
+  const handleCompanyLabel = (companyLabel) => {
+    setState((prevState) => ({ ...prevState, companyLabel }));
+  };
+
+  const handleRequisitesLabel = (requisitesLabel) => {
+    setState((prevState) => ({ ...prevState, requisitesLabel }));
+  };
+
   return (
     <aside>
-      <SelectOptions 
+      <SelectOptions
         placeholder={PLACEHOLDER_SELECT_OPTIONS}
         optionsArray={config}
-        handleSelect={setSelect}
-        logic={setLogic}
+        handleSelect={handleSelect}
+        logic={handleLogic}
       />
       {config.map((item, index) => (
-        (item.value && select.value === item.value) && <RadioGroup 
-          key={index}
-          optionsArray={item.options}
-          group={item.value}
-          handleChange={setCheck}
-          logic={setLogic}
-        />
+        (item.value && state.select.value === item.value) && (
+          <RadioGroup
+            key={index}
+            optionsArray={item.options}
+            group={item.value}
+            handleChange={handleCheck}
+            logic={handleLogic}
+          />
+        )
       ))}
-      {sharedValue.logic && <ConfigSection 
-        check={check}
-        titleLabel={titleLabel}
-        setTitleLabel={setTitleLabel}
-        companyLabel={companyLabel}
-        setCompanyLabel={setCompanyLabel}
-        requisitesLabel={requisitesLabel}
-        setRequisitesLabel={setRequisitesLabel}
-        companyName={companyName}
-        setCompanyName={setCompanyName}
-        profit={profit}
-        setProfit={setProfit}
-        round={round}
-        setRound={setRound}
-      />}
-      {sharedValue.logic && <>
-        {select.value === TYPE_PRICES && <Prices data={newData} value={check} />}
-        {select.value === TYPE_FLYERS && <Flyers data={newData} meta={metaData} value={check}/>}
-      </>}
+      {sharedValue.logic && (
+        <ConfigSection
+          check={state.check}
+          titleLabel={state.titleLabel}
+          setTitleLabel={handleTitleLabel}
+          companyLabel={state.companyLabel}
+          setCompanyLabel={handleCompanyLabel}
+          requisitesLabel={state.requisitesLabel}
+          setRequisitesLabel={handleRequisitesLabel}
+          companyName={state.companyName}
+          setCompanyName={handleCompanyName}
+          profit={state.profit}
+          setProfit={handleProfit}
+          round={state.round}
+          setRound={handleRound}
+        />
+      )}
+      {sharedValue.logic && (
+        <>
+          {state.select.value === TYPE_PRICES && <Prices data={newData} value={state.check} />}
+          {state.select.value === TYPE_FLYERS && <Flyers data={newData} meta={metaData} value={state.check} />}
+        </>
+      )}
     </aside>
-  )
+  );
 };
 
 export default SideBar;
