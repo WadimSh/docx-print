dcaf569867a1cc209b42472c77e71d520012122d
WadimSh <wadim11@yandex.ru>
2024-09-26 14:41:07 +0300

    fix

--- src/contexts/constant.js
+++ src/contexts/constant.js
@@ -2,7 +2,7 @@ export const DEFAULT_NAME_COMPANY = 'АО "Европа уно трейд"';
 export const LABEL_CHECKBOX_ROUND = 'сделать "красивое" округление';
 export const LABEL_BUTTONS_COUNTER = 'Ваша наценка, %';
 export const LABEL_CREATE_BUTTON = 'Создать документ';
-export const LABEL_DISABLED_BUTTON = 'Документ создается ...'
+export const LABEL_DISABLED_BUTTON = 'Документ создается ...';
 export const PLACEHOLDER_SELECT_OPTIONS = 'Выберите вид документа';
 export const LABEL_TEXT_INPUT = 'Введите наименование юридического лица';
 
